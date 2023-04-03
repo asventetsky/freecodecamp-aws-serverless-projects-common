a96f585fcc2f3051438be09abd9192b21c700867
asventetsky <artyom.sven@gmail.com>
2023-04-03 12:51:13 +0600

    terraform: fix error

--- terraform/module/aws/api_gateway/main.tf
+++ terraform/module/aws/api_gateway/main.tf
@@ -26,7 +26,7 @@ resource "aws_api_gateway_integration" "app" {
 
   rest_api_id = aws_api_gateway_rest_api.app.id
   resource_id = aws_api_gateway_method.app.resource_id
-  http_method = aws_api_gateway_method.app.http_method
+  http_method = aws_api_gateway_method.app[each.key].http_method
 
   integration_http_method = "POST"
   type = "AWS_PROXY"
